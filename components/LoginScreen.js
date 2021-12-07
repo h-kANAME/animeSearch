f5fb9670017f9d21fec428c3789fe62f7da74d57
h-kaname <luis--ao@hotmail.com>
2021-12-07 02:29:36 -0300

    fix layout

--- components/LoginScreen.js
+++ components/LoginScreen.js
@@ -1,13 +1,11 @@
 import React from 'react'
-import { Button } from 'react-native';
-//import * as Google from 'expo-google-app-auth';
+import { StyleSheet, View, Button } from 'react-native';
 import * as Google from 'expo-auth-session/providers/google';
 function LoginScreen({ navigation }) {
 
     const [request, response, promptAsync] = Google.useAuthRequest({
         expoClientId: '793559984832-8j3sa30s819dvleqt2dsu10065rj1mg6.apps.googleusercontent.com',
     });
-
 
     React.useEffect(() => {
         if (response?.type === 'success') {
@@ -17,12 +15,26 @@ function LoginScreen({ navigation }) {
     }, [response]);
 
     return (
-        <Button
-            disabled={!request}
-            title="Login"
-            onPress={() => { promptAsync(); }}
-        />
+        <View style={styles.container}>
+            <Button
+                disabled={!request}
+                title="Login"
+                onPress={() => { promptAsync(); }}
+            />
+        </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: 'center',
+        borderColor: "#CCC",
+        paddingTop: 7,
+        paddingBottom: 7,
+    },
+
+});
 
 export default LoginScreen;
